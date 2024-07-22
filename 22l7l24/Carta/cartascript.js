0cbbc7cc3093bff6cdc5e747f9ee5a593e22c69c
fedoraguy <49004207+FedoraGuyDev@users.noreply.github.com>
2024-07-22 02:04:08 -0300

    Add files via upload

---
+++ 22l7l24/Carta/cartascript.js
@@ -0,0 +1,10 @@
+  function playMusic(){
+      var music = document.getElementById("music");
+      music.play()
+  }
+  function stopMusic(){
+      var music = document.getElementById("music");
+      music.pause()
+  }
+  
+  
